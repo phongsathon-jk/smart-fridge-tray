6d0783b8310ca4467f180f06692645369973cf9a
phongsathon-jk <phongsathon.th@gmail.com>
2017-01-12 22:56:19 +0100

    initialize product database

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
-
 import { TabsPage } from '../pages/tabs/tabs';
-
+import { Storage } from '@ionic/storage';
 
 @Component({
   templateUrl: 'app.html'
@@ -11,12 +10,33 @@ import { TabsPage } from '../pages/tabs/tabs';
 export class MyApp {
   rootPage = TabsPage;
 
-  constructor(platform: Platform) {
+  constructor(platform: Platform, storage: Storage) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
+
+      /*
+       * Initialize the products.
+       * This is a database of all products those are recognised by the system.
+       * Scan an item's barcode then compare against the following products to get its detail.
+       */
+      storage.set('products', [
+        {
+          name: 'Apfelschorle 500 ml',
+          barcode: '1234567890',
+          weight_gram: 500,
+          expiry_date: '2017-12-31',
+          life_span: null
+        }, {
+          name: 'Apple',
+          barcode: null,
+          weight_gram: 1000,
+          expiry_date: null,
+          life_span_day: 20
+        }
+      ]);
     });
   }
 }
