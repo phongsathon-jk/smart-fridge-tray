e595ea536342a356a13932da5d496d50d4aabd8b
phongsathon-jk <phongsathon.th@gmail.com>
2017-01-12 20:09:19 +0100

    add project components

---
+++ src/pages/add-item/add-item.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-add-item',
+  templateUrl: 'add-item.html'
+})
+export class AddItemPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
---
+++ src/pages/view-all-items/view-all-items.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-view-all-items',
+  templateUrl: 'view-all-items.html'
+})
+export class ViewAllItemsPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
